eb3f03406c832000959352e383eb75a433b3f45f
Lea Hayes <lea.hayes@education.gov.uk>
2025-02-20 16:34:36 +0000

    Disable server header in Kestrel configuration.

--- src/Dfe.SignIn.PublicApi/Program.cs
+++ src/Dfe.SignIn.PublicApi/Program.cs
@@ -2,6 +2,10 @@ using Dfe.SignIn.PublicApi.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.WebHost.ConfigureKestrel(options => {
+    options.AddServerHeader = false;
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
