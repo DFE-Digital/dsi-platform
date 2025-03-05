dacd87df7f22a737d2c6f1c5dc8ff960203d9192
Richard Ward <richard.ward@methods.co.uk>
2025-03-05 09:59:00 +0000

    Updated examples for DI

--- src/Dfe.SignIn.PublicApi/Endpoints/SelectOrganisationEndpoints.cs
+++ src/Dfe.SignIn.PublicApi/Endpoints/SelectOrganisationEndpoints.cs
@@ -1,11 +1,28 @@
+
+using Dfe.SignIn.Core.Models.Applications.Interactions;
+using Dfe.SignIn.NodeApiClient;
+using Microsoft.AspNetCore.Mvc;
+
 namespace Dfe.SignIn.PublicApi.Endpoints;
 
 public static class SelectOrganisationEndpoints
 {
     public static void RegisterSelectOrganisationEndpoints(this WebApplication app)
     {
-        app.MapPost("select-organisation", () => {
-            return 0;
+        // Test with Applications
+        app.MapGet("select-applications", ([FromKeyedServices(NodeApiName.Applications)] HttpClient client) => {
+            return Results.Ok(client.BaseAddress);
+        });
+
+        // Test with Directories
+        app.MapGet("select-directories", ([FromKeyedServices(NodeApiName.Directories)] HttpClient client) => {
+            return Results.Ok(client.BaseAddress);
+        });
+
+        // Test using injected IGetClientByServiceId
+        app.MapGet("select-injected", async ([FromServices] IGetServiceApiSecretByServiceId service) => {
+            var result = await service.InvokeAsync(new GetServiceApiSecretByServiceIdRequest { ServiceId = Guid.Parse("") });
+            return Results.Json(result);
         });
     }
 }
