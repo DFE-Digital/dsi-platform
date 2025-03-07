ffbbec66a092ff559bb1b13e096a96f663e78caf
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-07 13:24:52 +0000

    Structure interaction configurations.

---
+++ src/Dfe.SignIn.SelectOrganisation.Web/Configuration/Interactions/SelectOrganisationExtensions.cs
@@ -0,0 +1,33 @@
+using Dfe.SignIn.Core.Framework;
+using Dfe.SignIn.Core.UseCases.SelectOrganisation;
+using Dfe.SignIn.SelectOrganisation.SessionData;
+
+namespace Dfe.SignIn.SelectOrganisation.Web.Configuration.Interactions;
+
+/// <exclude/>
+public static class SelectOrganisationExtensions
+{
+    /// <summary>
+    /// Setup "select organisation" interactions.
+    /// </summary>
+    /// <param name="services">The collection to add services to.</param>
+    /// <exception cref="ArgumentNullException">
+    ///   <para>If <paramref name="services"/> is null.</para>
+    /// </exception>
+    public static void SetupSelectOrganisationInteractions(this IServiceCollection services)
+    {
+        ArgumentNullException.ThrowIfNull(services, nameof(services));
+
+        SetupRedisSessionStore(services);
+
+        services.AddInteractor<GetSelectOrganisationSessionByKey_UseCase>();
+    }
+
+    private static void SetupRedisSessionStore(this IServiceCollection services)
+    {
+        services.AddStackExchangeRedisCache(options => {
+            options.Configuration = "localhost:6379";
+        });
+        services.AddSelectOrganisationSessionCache(options => { });
+    }
+}
