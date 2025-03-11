e67e4f35d0c6ae56dd54db6f3fda5f0058911812
Richard Ward <richard.ward@methods.co.uk>
2025-03-11 12:31:35 +0000

    Add GetOrganisationById and refactor

---
+++ src/Dfe.SignIn.NodeApiClient/Organisations/GetOrganisationById.cs
@@ -0,0 +1,32 @@
+using System.Net.Http.Json;
+using Dfe.SignIn.Core.Framework;
+using Dfe.SignIn.Core.Models.Organisations;
+using Dfe.SignIn.Core.Models.Organisations.Interactions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dfe.SignIn.NodeApiClient.Organisations;
+
+/// <summary>
+/// ApiRequester for obtaining an organisation by its unique identifier.
+/// </summary>
+/// <param name="httpClient"></param>
+[ApiRequester, NodeApi(NodeApiName.Organisations)]
+public sealed class GetOrganisationById_ApiRequester(
+    [FromKeyedServices(NodeApiName.Organisations)] HttpClient httpClient)
+    : IInteractor<GetOrganisationByIdRequest, GetOrganisationByIdResponse>
+{
+
+    /// <inheritdoc/>
+    public async Task<GetOrganisationByIdResponse> InvokeAsync(GetOrganisationByIdRequest request)
+    {
+        var response = await httpClient.GetFromJsonAsync<Models.OrganisationByIdDto>($"/organisations/{request.OrganisationId}");
+
+        return new GetOrganisationByIdResponse {
+            Organisation = response is null ? null : new OrganisationModel {
+                Id = response.Id,
+                LegalName = response.LegalName ?? string.Empty,
+                Name = response.Name
+            }
+        };
+    }
+}
--- src/Dfe.SignIn.NodeApiClient/Organisations/Models/OrganisationsAssociatedWithUserDto.cs
+++ src/Dfe.SignIn.NodeApiClient/Organisations/Models/OrganisationsAssociatedWithUserDto.cs
@@ -4,7 +4,7 @@ namespace Dfe.SignIn.NodeApiClient.Organisations.Models;
 
 internal sealed record OrganisationsAssociatedWithUserDto()
 {
-    public required OrganisationDto Organisation { get; init; }
+    public required UserOrganisationDto Organisation { get; init; }
     public RoleDto? Role { get; init; }
     public Guid[] Approvers { get; init; } = [];
     public Guid[] EndUsers { get; init; } = [];
@@ -12,49 +12,16 @@ internal sealed record OrganisationsAssociatedWithUserDto()
     public string? TextIdentifier { get; init; } = null;
 }
 
-internal sealed record OrganisationDto()
+internal sealed record UserOrganisationDto() : OrganisationDto
 {
-    [JsonPropertyName("id")]
-    public required Guid Id { get; init; }
-
-    [JsonPropertyName("name")]
-    public required string Name { get; init; }
-
-    [JsonPropertyName("LegalName")]
-    public string? LegalName { get; init; }
+    [JsonPropertyName("status")]
+    public StatusDto? Status { get; set; }
 
     [JsonPropertyName("category")]
     public CategoryDto? Category { get; init; }
 
-    [JsonPropertyName("status")]
-    public StatusDto? Status { get; set; }
-
     [JsonPropertyName("role")]
     public RoleDto? Role { get; init; }
-
-    [JsonPropertyName("urn")]
-    public string? Urn { get; set; }
-
-    [JsonPropertyName("uid")]
-    public string? Uid { get; set; }
-
-    [JsonPropertyName("upin")]
-    public string? Upin { get; set; }
-
-    [JsonPropertyName("ukprn")]
-    public string? Ukprn { get; set; }
-
-    [JsonPropertyName("establishmentNumber")]
-    public string? EstablishmentNumber { get; set; }
-
-    [JsonPropertyName("closedOn")]
-    public DateTime? ClosedOn { get; set; }
-
-    [JsonPropertyName("address")]
-    public string? Address { get; set; }
-
-    [JsonPropertyName("telephone")]
-    public string? Telephone { get; set; }
 
     [JsonPropertyName("statutoryLowAge")]
     public int? StatutoryLowAge { get; set; }
@@ -65,56 +32,11 @@ internal sealed record OrganisationDto()
     [JsonPropertyName("legacyId")]
     public long? LegacyId { get; set; }
 
+    [JsonPropertyName("telephone")]
+    public string? Telephone { get; set; }
+
     [JsonPropertyName("companyRegistrationNumber")]
     public string? CompanyRegistrationNumber { get; set; }
-
-    [JsonPropertyName("SourceSystem")]
-    public string? SourceSystem { get; set; }
-
-    [JsonPropertyName("providerTypeName")]
-    public string? ProviderTypeName { get; set; }
-
-    [JsonPropertyName("ProviderTypeCode")]
-    public int? ProviderTypeCode { get; set; }
-
-    [JsonPropertyName("GIASProviderType")]
-    public string? GIASProviderType { get; set; }
-
-    [JsonPropertyName("PIMSProviderType")]
-    public string? PIMSProviderType { get; set; }
-
-    [JsonPropertyName("PIMSProviderTypeCode")]
-    public int? PIMSProviderTypeCode { get; set; }
-
-    [JsonPropertyName("PIMSStatusName")]
-    public string? PIMSStatusName { get; set; }
-
-    [JsonPropertyName("pimsStatus")]
-    public int? PIMSStatus { get; set; }
-
-    [JsonPropertyName("GIASStatusName")]
-    public string? GIASStatusName { get; set; }
-
-    [JsonPropertyName("GIASStatus")]
-    public int? GIASStatus { get; set; }
-
-    [JsonPropertyName("MasterProviderStatusName")]
-    public string? MasterProviderStatusName { get; set; }
-
-    [JsonPropertyName("MasterProviderStatusCode")]
-    public int? MasterProviderStatusCode { get; set; }
-
-    [JsonPropertyName("OpenedOn")]
-    public string? OpenedOn { get; set; }
-
-    [JsonPropertyName("DistrictAdministrativeName")]
-    public string? DistrictAdministrativeName { get; set; }
-
-    [JsonPropertyName("DistrictAdministrativeCode")]
-    public string? DistrictAdministrativeCode { get; set; }
-
-    [JsonPropertyName("IsOnAPAR")]
-    public string? IsOnAPAR { get; set; }
 }
 
 internal sealed record RoleDto()
