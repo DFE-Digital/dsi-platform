1161665802dbd9c208c7e1f80746e92242bef8ba
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-12 00:44:05 +0000

    Add missing properties to fakes in unit tests.

--- src/Dfe.SignIn.PublicApi/Fakes/FakeFilterOrganisationsForUser.cs
+++ src/Dfe.SignIn.PublicApi/Fakes/FakeFilterOrganisationsForUser.cs
@@ -18,11 +18,13 @@ public sealed class FakeFilterOrganisationsForUser
                     Id = new Guid("a4412d34-6471-4663-8d70-73fe6617b5e5"),
                     Name = "Organisation A",
                     LegalName = "Legal name A",
+                    Status = 0,
                 },
                 new() {
                     Id = new Guid("561cdabf-d2f8-48f3-a66b-0f943837c9d7"),
                     Name = "Organisation B",
                     LegalName = "Legal name B",
+                    Status = 0,
                 },
             ],
         });
