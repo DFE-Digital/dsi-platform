3cf4d1758c66323087c41bd996841fdbab3e6bfc
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-12 00:08:45 +0000

    Use more consistent convention when testing for exceptions.

--- tests/Dfe.SignIn.NodeApiClient.UnitTests/HttpSecurityProvider/MsalHttpSecurityProviderTests.cs
+++ tests/Dfe.SignIn.NodeApiClient.UnitTests/HttpSecurityProvider/MsalHttpSecurityProviderTests.cs
@@ -8,24 +8,25 @@ namespace Dfe.SignIn.NodeApiClient.UnitTests.HttpSecurityProvider;
 [TestClass]
 public class MsalHttpSecurityProviderTests
 {
-
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void MsalHttpSecurityProvider_Throws_WhenScopesArgumentIsNull()
     {
         var mockIConfidentialClientApplication = new Mock<IConfidentialClientApplication>();
 
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-        new MsalHttpSecurityProvider(null, mockIConfidentialClientApplication.Object);
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        new MsalHttpSecurityProvider(
+            scopes: null!,
+            confidentialClientApplication: mockIConfidentialClientApplication.Object
+        );
     }
 
     [TestMethod]
     [ExpectedException(typeof(ArgumentNullException))]
     public void MsalHttpSecurityProvider_Throws_WhenConfidentialClientApplicationArgumentIsNull()
     {
-#pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
-        new MsalHttpSecurityProvider([], null);
-#pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+        new MsalHttpSecurityProvider(
+            scopes: [],
+            confidentialClientApplication: null!
+        );
     }
 }
