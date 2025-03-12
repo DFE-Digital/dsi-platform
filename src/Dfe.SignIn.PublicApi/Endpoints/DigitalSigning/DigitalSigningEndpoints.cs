8b4efbc9dfd373c1446301bfaf2513803c58d3f0
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-12 16:51:13 +0000

    Return public keys with 'application/json' content type.
    Annotate with type representing the data payload so that it is shown properly in the Swagger UI.

--- src/Dfe.SignIn.PublicApi/Endpoints/DigitalSigning/DigitalSigningEndpoints.cs
+++ src/Dfe.SignIn.PublicApi/Endpoints/DigitalSigning/DigitalSigningEndpoints.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics.CodeAnalysis;
+using Dfe.SignIn.Core.PublicModels.PublicApiSigning;
 using Dfe.SignIn.PublicApi.Configuration;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 
 namespace Dfe.SignIn.PublicApi.Endpoints.DigitalSigning;
@@ -22,13 +24,15 @@ public static class DigitalSigningEndpoints
     /// <summary>
     /// Gets the list of public keys to verify digitally signed payloads.
     /// </summary>
-    public static string GetKeys(
+    [Produces("application/json", Type = typeof(WellKnownPublicKeyListing))]
+    public static IResult GetKeys(
         IOptions<ApplicationOptions> optionsAccessor)
     {
         string publicKeysJson = optionsAccessor.Value.PublicKeysJson;
         if (string.IsNullOrWhiteSpace(publicKeysJson)) {
             throw new InvalidOperationException("Missing configuration 'ApplicationOptions.PublicKeysJson'.");
         }
-        return publicKeysJson;
+
+        return Results.Text(publicKeysJson, "application/json");
     }
 }
