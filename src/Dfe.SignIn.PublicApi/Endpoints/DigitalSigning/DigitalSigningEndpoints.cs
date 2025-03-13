d83488b80d2d2963916b32b7e77f19892aca1419
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-13 14:16:49 +0000

    Reorganise endpoints using partial classes so that there is one .cs file per endpoint.

--- src/Dfe.SignIn.PublicApi/Endpoints/DigitalSigning/DigitalSigningEndpoints.cs
+++ src/Dfe.SignIn.PublicApi/Endpoints/DigitalSigning/DigitalSigningEndpoints.cs
@@ -1,15 +1,11 @@
 using System.Diagnostics.CodeAnalysis;
-using Dfe.SignIn.Core.PublicModels.PublicApiSigning;
-using Dfe.SignIn.PublicApi.Configuration;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Options;
 
 namespace Dfe.SignIn.PublicApi.Endpoints.DigitalSigning;
 
 /// <summary>
 /// Endpoints for the digital signing feature.
 /// </summary>
-public static class DigitalSigningEndpoints
+public static partial class DigitalSigningEndpoints
 {
     /// <summary>
     /// Registers all endpoints for the digital signing feature.
@@ -19,20 +15,5 @@ public static class DigitalSigningEndpoints
     public static void UseDigitalSigningEndpoints(this IEndpointRouteBuilder app)
     {
         app.MapGet("v2/.well-known/keys", GetKeys);
-    }
-
-    /// <summary>
-    /// Gets the list of public keys to verify digitally signed payloads.
-    /// </summary>
-    [Produces("application/json", Type = typeof(WellKnownPublicKeyListing))]
-    public static IResult GetKeys(
-        IOptions<ApplicationOptions> optionsAccessor)
-    {
-        string publicKeysJson = optionsAccessor.Value.PublicKeysJson;
-        if (string.IsNullOrWhiteSpace(publicKeysJson)) {
-            throw new InvalidOperationException("Missing configuration 'ApplicationOptions.PublicKeysJson'.");
-        }
-
-        return Results.Text(publicKeysJson, "application/json");
     }
 }
