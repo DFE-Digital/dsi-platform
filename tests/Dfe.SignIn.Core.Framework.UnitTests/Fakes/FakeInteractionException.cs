254c6e8977df0d810a32089374cd4f525f9c1019
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-18 11:59:42 +0000

    Add serialization functionality for exception types.

--- tests/Dfe.SignIn.Core.Framework.UnitTests/Fakes/FakeInteractionException.cs
+++
@@ -1,4 +0,0 @@
-namespace Dfe.SignIn.Core.Framework.UnitTests.Fakes;
-
-public sealed class FakeInteractionException(string? message = null, Exception? innerException = null)
-    : InteractionException(message, innerException);
