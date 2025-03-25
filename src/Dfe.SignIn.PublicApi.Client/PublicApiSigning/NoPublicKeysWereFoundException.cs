c2f659160b0ff4ffd826cfb82e7e1167e4a53d81
Lea Hayes <lea.hayes@education.gov.uk>
2025-03-25 14:19:01 +0000

    Annotate custom exception types with `SerializableAttribute`.

--- src/Dfe.SignIn.PublicApi.Client/PublicApiSigning/NoPublicKeysWereFoundException.cs
+++ src/Dfe.SignIn.PublicApi.Client/PublicApiSigning/NoPublicKeysWereFoundException.cs
@@ -4,6 +4,7 @@ namespace Dfe.SignIn.PublicApi.Client.PublicApiSigning;
 /// The exception thrown when no public keys were retrieved when attempting to refresh
 /// public keys from the DfE Sign-in public API.
 /// </summary>
+[Serializable]
 public sealed class NoPublicKeysWereFoundException : Exception
 {
     /// <inheritdoc/>
