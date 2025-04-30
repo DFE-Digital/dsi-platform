67258eaeaf4c62996111813c56615da13dbc5575
Richard Ward <richard.ward@methods.co.uk>
2025-04-30 09:06:03 +0100

    /healthcheck url should be /v2/healthcheck
    After this change, all dsi-platform tests continue to complete successfully.

--- src/Dfe.SignIn.SelectOrganisation.Web/Configuration/HealthCheckExtensions.cs
+++ src/Dfe.SignIn.SelectOrganisation.Web/Configuration/HealthCheckExtensions.cs
@@ -40,11 +40,11 @@ public static class HealthCheckExtensions
     /// Expose the heathcheck via an endpoint.
     /// </summary>
     /// <param name="builder">The builder to register the healthchecks on.</param>
-    /// <param name="endpoint">The endpoint which healthchecks will be made available, defaulting to '/healthcheck'.</param>
+    /// <param name="endpoint">The endpoint which healthchecks will be made available, defaulting to '/v2/healthcheck'.</param>
     /// <exception cref="ArgumentNullException">
     ///   <para>If <paramref name="builder"/> is null.</para>
     /// </exception>
-    public static void UseHealthChecks(this IApplicationBuilder builder, string endpoint = "/healthcheck")
+    public static void UseHealthChecks(this IApplicationBuilder builder, string endpoint = "/v2/healthcheck")
     {
         ArgumentNullException.ThrowIfNull(builder, nameof(builder));
 
