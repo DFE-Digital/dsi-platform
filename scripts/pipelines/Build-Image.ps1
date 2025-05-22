80819dd5866dddc554cdce98f8d9b9b03a0c9e33
Lea Hayes <lea.hayes@education.gov.uk>
2025-05-22 16:45:04 +0100

    idea

--- scripts/pipelines/Build-Image.ps1
+++ scripts/pipelines/Build-Image.ps1
@@ -2,13 +2,13 @@
 param(
     [string]$regServer,
     [string]$appName,
-    [string]$target,
+    [string]$targetWorkflow,
     [string]$buildId,
     [string]$dockerFileName,
     [string]$dotNetProjName = ""
 )
 
-$tag = "{0}/{1}:{2}-{3}" -f $regServer,$($appName.ToLower()),$($target.ToLower()),$buildId
+$tag = "{0}/{1}:{2}-{3}" -f $regServer,$($appName.ToLower()),$($targetWorkflow.ToLower()),$buildId
 
 az acr login --name $regServer
 docker build -t $tag --file "./docker/Linux-app-service/$dockerFileName.dockerfile" --build-arg PROC_FILE_NAME=$dotNetProjName . 
