c0984b007820e930cc1082758fb14d290a44d4a2
Lea Hayes <lea.hayes@education.gov.uk>
2025-06-02 17:11:05 +0100

    tweak

---
+++ docker/dotnet-component/init.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+dotnet /home/site/wwwroot/${FILE_NAME}.dll
---
+++ scripts/actions/Build-DotNetImage.ps1
@@ -0,0 +1,38 @@
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [string]$RegistryServer,
+
+    [Parameter(Mandatory=$true)]
+    [string]$repositoryName,
+
+    [Parameter(Mandatory=$true)]
+    [string]$Lifecycle,
+
+    [Parameter(Mandatory=$true)]
+    [string]$RunId,
+
+    [Parameter(Mandatory=$true)]
+    [string]$RunNumber,
+
+    [Parameter(Mandatory=$true)]
+    [string]$DotNetProjectName
+)
+
+$ErrorActionPreference = "Stop"
+
+$imageMappings = @{
+    "Dfe.SignIn.Web.SelectOrganisation" = "select-organisation"
+    "Dfe.SignIn.PublicApi" = "public-api-v2"
+}
+
+$repositoryName = $imageMappings[$DotNetProjectName]
+if (-not $repositoryName) {
+    throw "Repository mapping not specified for '$DotNetProjectName'."
+}
+
+$tag = "{0}/{1}:{2}-{3}-{4}" -f $RegistryServer, $($repositoryName.ToLower()), $Lifecycle, $RunId, $RunNumber
+
+# az acr login --name $RegistryServer
+docker build -t $tag --file ./docker/dotnet-component/Dockerfile --build-arg PROC_FILE_NAME=$DotnetProjectName .
+# docker push "$RegistryServer/$repositoryName" --all-tags
