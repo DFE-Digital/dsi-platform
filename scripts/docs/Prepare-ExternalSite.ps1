7389602c5d0f515a17f47fc15ad10e168f4a30a3
Richard Ward <richard.ward@methods.co.uk>
2025-06-04 10:22:40 +0100

    Remove `Ruby` from both scripts, as it now exist only in `External`

--- scripts/docs/Prepare-ExternalSite.ps1
+++ scripts/docs/Prepare-ExternalSite.ps1
@@ -1,4 +1,3 @@
 $docsPath = Resolve-Path "${PSScriptRoot}/../../docs"
 
 Copy-Item -Path "$docsPath/api" -Destination "$docsPath/external" -recurse -Force
-Copy-Item -Path "$docsPath/ruby" -Destination "$docsPath/external" -recurse -Force
