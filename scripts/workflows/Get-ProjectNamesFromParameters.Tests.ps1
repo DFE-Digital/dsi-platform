e33289c66d69a103b171fbc954ba141d32e16193
Lea Hayes <lea.hayes@education.gov.uk>
2025-06-12 01:59:12 +0100

    Add main CI workflow.

---
+++ scripts/workflows/Get-ProjectNamesFromParameters.Tests.ps1
@@ -0,0 +1,55 @@
+BeforeAll {
+    $Cmdlet = $PSCommandPath.Replace('.Tests.ps1','.ps1')
+}
+
+Describe "Get-ProjectNamesFromParameters" {
+    Context "when no inputs are given" {
+        It "should return no project names" {
+            $result = & $Cmdlet
+
+            $result | Should -Be @()
+        }
+    }
+
+    Context "when '<InputName>' input is given" -ForEach @(
+        @{
+            InputName = 'SelectOrganisation'
+            ExpectedProjectName = 'Dfe.SignIn.Web.SelectOrganisation'
+        }
+    ) {
+        It "should return the expected project name when input is 'true'" {
+            $params = @{ $InputName = 'true' }
+            $result = & $Cmdlet @params
+
+            $result | Should -Be @( $ExpectedProjectName )
+        }
+
+        It "should not return the expected project name when input is 'false'" {
+            $params = @{ $InputName = 'false' }
+            $result = & $Cmdlet @params
+
+            $result | Should -Be @()
+        }
+    }
+
+    Context "when multiple inputs are given" {
+        It "should return the expected project names when some are true" {
+            $result = & $Cmdlet `
+                -SelectOrganisation 'true' `
+                -PublicApi 'false'
+
+            $result | Should -Be @( 'Dfe.SignIn.Web.SelectOrganisation' )
+        }
+
+        It "should return the expected project names when all are true" {
+            $result = & $Cmdlet `
+                -SelectOrganisation 'true' `
+                -PublicApi 'true'
+
+            $result | Sort-Object | Should -Be @(
+                'Dfe.SignIn.PublicApi'
+                'Dfe.SignIn.Web.SelectOrganisation'
+            )
+        }
+    }
+}
---
+++ scripts/workflows/Set-OutputFlags.ps1
@@ -0,0 +1,58 @@
+<#
+.SYNOPSIS
+    Sets output flags for the current job step.
+
+.PARAMETER ChangedFiles
+    The array of changed files.
+
+.PARAMETER Flags
+    An object where each key specifies a flag that is to be set.
+
+    The following options can be specified for each flag:
+        - Input - String. When 'true' indicates that flag should be set.
+        - Patterns - String[]. An array of file patterns.
+
+.OUTPUTS
+    Writes flags to GitHub output and outputs a summary to the host.
+
+.EXAMPLE
+    ./scripts/workflows/Set-OutputFlags -ChangedFiles $changedFiles -Flags @{
+      flag1 = @{
+        Input = '${{ github.events.inputs.input_override1 }}'
+        Patterns = "^src/"
+      }
+      flag2 = @{
+        Input = '${{ github.events.inputs.input_override2 }}'
+        Patterns = @(
+            "^src/Project1/"
+            "^src/Project2/"
+        )
+      }
+    }
+#>
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory=$true)]
+    [AllowEmptyCollection()]
+    [String[]]$ChangedFiles,
+
+    [Parameter(Mandatory=$true)]
+    [PSCustomObject]$Flags
+)
+
+$ErrorActionPreference = "Stop"
+
+foreach ($flag in $Flags.GetEnumerator()) {
+    $value = $flag.Value.Input -eq 'true'
+
+    if ($flag.Value.Patterns) {
+        $pattern = $flag.Value.Patterns -join '|'
+        $value = $value -or $ChangedFiles.Where({ $_ -match $pattern }, 'First').Count -gt 0
+    }
+
+    $value = "$value".ToLower()
+
+    Write-Host "Set flag '$($flag.Key)=$value'"
+
+    Add-Content -Path $env:GITHUB_OUTPUT -Value "$($flag.Key)=$value"
+}
