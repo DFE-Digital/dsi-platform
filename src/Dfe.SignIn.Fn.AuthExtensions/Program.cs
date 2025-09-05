52061759ad4bf44f495c00434be359a64fdcad6a
Lea Hayes <lea.hayes@education.gov.uk>
2025-09-05 10:37:58 +0100

    Create empty Azure Function project for 'Dfe.SignIn.Fn.AuthExtensions'.

---
+++ src/Dfe.SignIn.Fn.AuthExtensions/Program.cs
@@ -0,0 +1,18 @@
+using Dfe.SignIn.Base.Framework;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = FunctionsApplication.CreateBuilder(args);
+
+builder.ConfigureFunctionsWebApplication();
+
+builder.Services
+    .AddApplicationInsightsTelemetryWorkerService()
+    .ConfigureFunctionsApplicationInsights();
+
+builder.Services
+    .AddInteractionFramework();
+
+builder.Build().Run();
---
+++ tests/Dfe.SignIn.Fn.AuthExtensions.UnitTests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace Dfe.SignIn.Fn.AuthExtensions.UnitTests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
