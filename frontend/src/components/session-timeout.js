cc94781984d6996d68c231a94e0123daefe0b41e
Lea Hayes <lea.hayes@education.gov.uk>
2025-12-19 11:41:49 +0000

    Consistent use of quote characters in frontend scripts.

--- frontend/src/components/session-timeout.js
+++ frontend/src/components/session-timeout.js
@@ -1,5 +1,5 @@
-const tabId = Number(window.localStorage.getItem('session-timeout:next-id'));
-window.localStorage.setItem('session-timeout:next-id', tabId + 1);
+const tabId = Number(window.localStorage.getItem("session-timeout:next-id"));
+window.localStorage.setItem("session-timeout:next-id", tabId + 1);
 
 function initSessionTimeoutElement(element) {
   const sessionDurationInMinutes = parseInt(element.dataset.sessionDurationInMinutes);
@@ -7,19 +7,19 @@ function initSessionTimeoutElement(element) {
   const endSessionUrl = element.dataset.endSessionUrl;
   const timeoutUrl = element.dataset.timeoutUrl;
 
-  const modalElement = element.querySelector('.app-session-timeout__modal');
-  const timerElement = element.querySelector('.app-session-timeout__timer');
-  const minutesElement = element.querySelector('.app-session-timeout__minutes');
-  const secondsElement = element.querySelector('.app-session-timeout__seconds');
-  const timeoutNowElement = element.querySelector('.app-session-timeout__now');
-  const extendButtonElement = element.querySelector('.app-session-timeout__extend');
+  const modalElement = element.querySelector(".app-session-timeout__modal");
+  const timerElement = element.querySelector(".app-session-timeout__timer");
+  const minutesElement = element.querySelector(".app-session-timeout__minutes");
+  const secondsElement = element.querySelector(".app-session-timeout__seconds");
+  const timeoutNowElement = element.querySelector(".app-session-timeout__now");
+  const extendButtonElement = element.querySelector(".app-session-timeout__extend");
 
   extendButtonElement.addEventListener("click", () => location.reload());
 
-  window.localStorage.setItem('session-timeout:end', Date.now() + sessionDurationInMinutes * 60 * 1000);
+  window.localStorage.setItem("session-timeout:end", Date.now() + sessionDurationInMinutes * 60 * 1000);
 
   const intervalId = setInterval(() => {
-    const timeout = Number(window.localStorage.getItem('session-timeout:end'));
+    const timeout = Number(window.localStorage.getItem("session-timeout:end"));
     if (timeout === 1) {
       setTimeout(() => {
         location.href = timeoutUrl;
@@ -30,8 +30,8 @@ function initSessionTimeoutElement(element) {
 
     const popupTime = timeout - notifyRemainingMinutes * 60 * 1000;
     if (Date.now() >= popupTime) {
-      if (!element.classList.contains('app-session-timeout--show')) {
-        element.classList.add('app-session-timeout--show');
+      if (!element.classList.contains("app-session-timeout--show")) {
+        element.classList.add("app-session-timeout--show");
         if (modalElement.showModal) {
           modalElement.showModal();
         }
@@ -49,11 +49,11 @@ function initSessionTimeoutElement(element) {
         timerElement.style.display = "none";
         timeoutNowElement.style.display = "";
 
-        window.localStorage.setItem('session-timeout:closer', tabId);
+        window.localStorage.setItem("session-timeout:closer", tabId);
         setTimeout(() => {
           // Allow one tab to lead; and then all of the others can follow shortly after.
           // Avoid scenario where all tabs initiate the sign out.
-          if (tabId === Number(window.localStorage.getItem('session-timeout:closer'))) {
+          if (tabId === Number(window.localStorage.getItem("session-timeout:closer"))) {
             location.href = endSessionUrl;
           }
           else {
@@ -68,12 +68,12 @@ function initSessionTimeoutElement(element) {
 
         timerElement.style.display = "";
         timeoutNowElement.style.display = "none";
-        minutesElement.textContent = minutesRemaining > 0 ? minutesRemaining + ' minutes' : '';
-        secondsElement.textContent = secondsRemaining + ' seconds';
+        minutesElement.textContent = minutesRemaining > 0 ? minutesRemaining + " minutes" : "";
+        secondsElement.textContent = secondsRemaining + " seconds";
       }
     }
     else {
-      element.classList.remove('app-session-timeout--show');
+      element.classList.remove("app-session-timeout--show");
       if (modalElement.showModal) {
         modalElement.close();
       }
@@ -82,5 +82,5 @@ function initSessionTimeoutElement(element) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll('.app-session-timeout').forEach(initSessionTimeoutElement);
+  document.querySelectorAll(".app-session-timeout").forEach(initSessionTimeoutElement);
 });
