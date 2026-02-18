cabdbb13cb072fc6613c85fb8497be818f6d70a5
Ionut Predoi <ionut.predoi@education.gov.uk>
2026-02-18 14:33:26 +0000

    Rearrange UseUserEndpoints in alphabetical order.

--- src/Dfe.SignIn.InternalApi/Endpoints/InteractionEndpoints.cs
+++ src/Dfe.SignIn.InternalApi/Endpoints/InteractionEndpoints.cs
@@ -57,11 +57,11 @@ public static partial class InteractionEndpoints
     public static void UseUserEndpoints(this WebApplication app)
     {
         app.Map<AutoLinkEntraUserToDsiRequest, AutoLinkEntraUserToDsiResponse>();
-        app.Map<LinkEntraUserToDsiRequest, LinkEntraUserToDsiResponse>();
         app.Map<ChangeJobTitleRequest, ChangeJobTitleResponse>();
         app.Map<CheckIsBlockedEmailAddressRequest, CheckIsBlockedEmailAddressResponse>();
         app.Map<GetOrganisationsAssociatedWithUserRequest, GetOrganisationsAssociatedWithUserResponse>();
         app.Map<GetUserProfileRequest, GetUserProfileResponse>();
         app.Map<GetUserStatusRequest, GetUserStatusResponse>();
+        app.Map<LinkEntraUserToDsiRequest, LinkEntraUserToDsiResponse>();
     }
 }
