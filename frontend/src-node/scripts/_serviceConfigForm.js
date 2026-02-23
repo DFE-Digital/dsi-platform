546644fbaa11f1a305ba6d5e8b9dd26506980890
vsolomonadv <victor.solomon@advania.co.uk>
2026-02-23 16:36:55 +0000

    test: add updateSections jest tests and fix lint issues

--- frontend/src-node/scripts/_serviceConfigForm.js
+++ frontend/src-node/scripts/_serviceConfigForm.js
@@ -5,6 +5,83 @@ const FLOW_TYPE_IMPLICIT = "implicit";
 const FLOW_TYPE_AUTHORIZATION = "authorization";
 const FLOW_TYPE_HYBRID = "hybrid";
 
+function updateSections() {
+  const selectedTypes = [];
+
+  if ($("#response_types-code").is(":checked")) {
+    selectedTypes.push(RESPONSE_TYPE_CODE);
+  }
+  if ($("#response_types-id_token").is(":checked")) {
+    selectedTypes.push(RESPONSE_TYPE_ID_TOKEN);
+  }
+
+  // 1. DETERMINE FLOW TYPE
+  let flowType = "";
+  let warningMessage = "";
+
+  if (selectedTypes.length === 0) {
+    flowType = "";
+  }
+  else if (selectedTypes.length === 1 && selectedTypes.includes(RESPONSE_TYPE_CODE)) {
+    flowType = FLOW_TYPE_AUTHORIZATION;
+    warningMessage = `
+      <div class="govuk-warning-text govuk-!-margin-top-5 govuk-!-margin-bottom-0" id="warning-response-types">
+          <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
+          <strong class="govuk-warning-text__text">
+              <span class="govuk-warning-text__assistive">Warning</span>
+              You have selected code as your response type. This means you are using Authorization Code Flow.
+          </strong>
+      </div>
+    `;
+  }
+  else if (selectedTypes.length === 2) {
+    // Hybrid
+    flowType = FLOW_TYPE_HYBRID;
+    warningMessage = `
+      <div class="govuk-warning-text govuk-!-margin-top-5 govuk-!-margin-bottom-0" id="warning-response-types">
+          <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
+          <strong class="govuk-warning-text__text">
+              <span class="govuk-warning-text__assistive">Warning</span>
+              You have selected code and id_token. This means Hybrid Flow is used.
+          </strong>
+      </div>
+    `;
+  }
+  else {
+    // id_token only (implicit)
+    flowType = FLOW_TYPE_IMPLICIT;
+  }
+
+  // 2. UPDATE WARNING DISPLAY
+  $("#warning-response-types").remove();
+
+  if (warningMessage) {
+    $("#response_types-fieldset").append(warningMessage);
+  }
+
+  // 3. ENABLE/DISABLE FIELDS BASED ON FLOW TYPE
+  const shouldEnable = (
+    flowType === FLOW_TYPE_AUTHORIZATION ||
+    flowType === FLOW_TYPE_HYBRID
+  );
+
+  if (shouldEnable) {
+    $("#refresh_token-wrapper :input, #clientSecret-wrapper :input, #tokenEndpointAuthMethod-wrapper select")
+      .prop("disabled", false);
+
+    $("#refresh_token-wrapper, #clientSecret-wrapper, #tokenEndpointAuthMethod-wrapper")
+      .slideDown(500);
+  } else {
+    $("#refresh_token-wrapper, #clientSecret-wrapper, #tokenEndpointAuthMethod-wrapper")
+      .slideUp(500, () => {
+
+        $("#refresh_token-wrapper :input, #clientSecret-wrapper :input, #tokenEndpointAuthMethod-wrapper select")
+          .prop("disabled", true);
+
+      });
+  }
+}
+
 $(() => {
   const createServiceConfigUrlSections = (
     sectionId,
@@ -84,110 +161,18 @@ $(() => {
     "Are you sure you want to regenerate the API secret?",
   );
 
-  function updateSections() {
-    const selectedTypes = [];
-
-
-    if ($("#response_types-code").is(":checked")) {
-      selectedTypes.push(RESPONSE_TYPE_CODE);
-    }
-
-    let warningMessage = "";
-
-    if (selectedTypes.length > 0) {
-      selectedTypes.sort((a, b) => {
-        const order = [
-          RESPONSE_TYPE_CODE,
-          RESPONSE_TYPE_ID_TOKEN,
-        ];
-        return order.indexOf(a) - order.indexOf(b);
-      });
-
-      let flowType = FLOW_TYPE_IMPLICIT;
-
-      if (
-        selectedTypes.length === 1 &&
-        selectedTypes.includes(RESPONSE_TYPE_CODE)
-      ) {
-        flowType = FLOW_TYPE_AUTHORIZATION;
-      } else if (
-
-        (selectedTypes.includes(RESPONSE_TYPE_CODE) &&
-          selectedTypes.includes(RESPONSE_TYPE_ID_TOKEN))
-      ) {
-        flowType = FLOW_TYPE_HYBRID;
-      }
-
-      let selectedTypesDisplay;
-
-      if (selectedTypes.length > 1) {
-        const allButLast = selectedTypes.slice(0, -1).join(", ");
-        const last = selectedTypes[selectedTypes.length - 1];
-        selectedTypesDisplay = `${allButLast} and ${last}`;
-      } else {
-        [selectedTypesDisplay] = selectedTypes;
-      }
-
-      warningMessage = `
-            <div class="govuk-warning-text govuk-!-margin-top-5 govuk-!-margin-bottom-0" id="warning-response-types">
-                <span class="govuk-warning-text__icon" aria-hidden="true">!</span>
-                <strong class="govuk-warning-text__text">
-                    <span class="govuk-warning-text__assistive">Warning</span>
-                    You have selected ${selectedTypesDisplay} as your response type. This means ${flowType} flow is your flow.
-                </strong>
-            </div>
-      `;
-  }
-
-  // Remove existing warning, if any
-  $("#warning-response-types").remove();
-
-  // Add the new warning
-  if (warningMessage) {
-    $("#response_types-fieldset").append(warningMessage);
-  }
-  const responseTypesCodeElement = $("#response_types-code");
-  if (responseTypesCodeElement.length) {
-    const initialOffset = $("#response_types-code").offset().top;
-
-    const initialScrollPosition = $(window).scrollTop();
-
-    if ($("#response_types-code").is(":checked")) {
-      $(
-        "#refresh_token-wrapper :input, #clientSecret-wrapper :input, #tokenEndpointAuthMethod-wrapper select",
-      ).prop("disabled", false);
-      $(
-        "#refresh_token-wrapper, #clientSecret-wrapper, #tokenEndpointAuthMethod-wrapper",
-      ).slideDown(500);
-    } else {
-      $(
-        "#refresh_token-wrapper, #clientSecret-wrapper, #tokenEndpointAuthMethod-wrapper",
-      ).slideUp(500, () => {
-        $(
-          "#refresh_token-wrapper :input, #clientSecret-wrapper :input, #tokenEndpointAuthMethod-wrapper select",
-        ).prop("disabled", true);
-      });
-    }
-
-    const newOffset = $("#response_types-code").offset().top;
-    const offsetDifference = newOffset - initialOffset;
-
-    $("html, body").animate(
-      {
-        scrollTop: initialScrollPosition + offsetDifference,
-      },
-      50,
-      "linear",
-    );
-  }
-}
-updateSections();
-
-// Event listener for the checkboxes
-$("#response_types-id_token, #response_types-code").on(
-  "change",
-  () => {
+  // Only auto-run in a real browser (not under Jest/Node)
+  if (typeof document !== 'undefined' && typeof window !== 'undefined') {
     updateSections();
-    }
-  );
+  }
+
+  // Event listener for the checkboxes
+  $("#response_types-id_token, #response_types-code").on("change", () => {
+    updateSections();
+  });
+
 });
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = { updateSections };
+}
