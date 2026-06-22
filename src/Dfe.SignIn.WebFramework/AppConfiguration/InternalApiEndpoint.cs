c6a78de5c8f9f7139103ba687cb697ed15e20f0e
John Rudd <john.rudd@education.gov.uk>
2026-06-22 11:53:13 +0100

    Fixed missing documentation link

--- src/Dfe.SignIn.WebFramework/AppConfiguration/InternalApiEndpoint.cs
+++ src/Dfe.SignIn.WebFramework/AppConfiguration/InternalApiEndpoint.cs
@@ -2,8 +2,14 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Dfe.SignIn.WebFramework.AppConfiguration;
 
+/// <summary>
+/// Internal Api configuration with attribute validation
+/// </summary>
 public class InternalApiEndpoint
 {
+    /// <summary>
+    /// Base resource address
+    /// </summary>
     [Required, Url]
     public string? BaseAddress { get; set; }
 }
