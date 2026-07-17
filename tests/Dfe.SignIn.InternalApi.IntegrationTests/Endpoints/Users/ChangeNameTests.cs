23c7e74ee51fdb803ff721d44fa88018c72f26e1
Amjad Mahmood <amjad.mahmood@education.gov.uk>
2026-07-17 09:32:44 +0100

    Split to firstname and lastname test

--- tests/Dfe.SignIn.InternalApi.IntegrationTests/Endpoints/Users/ChangeNameTests.cs
+++ tests/Dfe.SignIn.InternalApi.IntegrationTests/Endpoints/Users/ChangeNameTests.cs
@@ -215,26 +215,40 @@ public class ChangeNameTests : InternalApiIntegrationEndpointTestBase
     }
 
     [Theory]
-    [InlineData("", "Smith")]         // Invalid FirstName: Empty
-    [InlineData("Jane!!!", "Smith")]  // Invalid FirstName: Special chars
-    [InlineData("Jane", "")]          // Invalid LastName: Empty
-    [InlineData("Jane", "Smith!!!")]  // Invalid LastName: Special chars
-    public async Task ChangeName_Returns400_WhenNameIsInvalid(string firstName, string lastName)
+    [InlineData("")]
+    [InlineData("Jane!!!")]
+    public async Task ChangeName_Returns400_WhenFirstNameIsInvalid(string invalidFirstName)
     {
-        var (authenticatedClient, auditMock) = this.CreateClientWithAuditMock();
-
+        var (authenticatedClient, _) = this.CreateClientWithAuditMock();
         var user = EntityFaker.User.Generate();
-
         await this.InsertEntityAsync<DbDirectoriesContext, UserEntity>(user);
 
         var request = new ChangeNameRequest {
             UserId = user.Sub,
-            FirstName = firstName,
-            LastName = lastName
+            FirstName = invalidFirstName,
+            LastName = "Smith"
         };
 
         var response = await authenticatedClient.PostAsJsonAsync(endpoint, request);
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+    }
 
+    [Theory]
+    [InlineData("")]
+    [InlineData("Smith!!!")]
+    public async Task ChangeName_Returns400_WhenLastNameIsInvalid(string invalidLastName)
+    {
+        var (authenticatedClient, _) = this.CreateClientWithAuditMock();
+        var user = EntityFaker.User.Generate();
+        await this.InsertEntityAsync<DbDirectoriesContext, UserEntity>(user);
+
+        var request = new ChangeNameRequest {
+            UserId = user.Sub,
+            FirstName = "Jane",
+            LastName = invalidLastName
+        };
+
+        var response = await authenticatedClient.PostAsJsonAsync(endpoint, request);
         Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
