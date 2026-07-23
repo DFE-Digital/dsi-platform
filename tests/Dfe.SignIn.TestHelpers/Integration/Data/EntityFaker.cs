1b5a92969c31d30d4525f144f8fdb28674866bb2
Amjad Mahmood <amjad.mahmood@education.gov.uk>
2026-07-23 10:17:49 +0100

    fixed formatting

--- tests/Dfe.SignIn.TestHelpers/Integration/Data/EntityFaker.cs
+++ tests/Dfe.SignIn.TestHelpers/Integration/Data/EntityFaker.cs
@@ -9,22 +9,22 @@ namespace Dfe.SignIn.TestHelpers.Integration.Data;
 public static class EntityFaker
 {
     public static Faker<UserEntity> User => new Faker<UserEntity>()
-        .RuleFor( x => x.Sub, f => f.Random.Guid() )
-        .RuleFor( x => x.Email, f => f.Internet.Email() )
-        .RuleFor( x => x.FirstName, f => f.Name.FirstName() )
-        .RuleFor( x => x.LastName, f => f.Name.LastName() )
-        .RuleFor( x => x.Password, f => f.Internet.Password() )
-        .RuleFor( x => x.Salt, f => f.Random.AlphaNumeric( 32 ) )
-        .RuleFor( x => x.Status, _ => (short)AccountStatus.Active )
-        .RuleFor( x => x.CreatedAt, f => f.Date.Past( 2 ) )
-        .RuleFor( x => x.UpdatedAt, ( f, user ) => f.Date.Between( user.CreatedAt, DateTime.UtcNow ) )
-        .RuleFor( x => x.JobTitle, ( _, _ ) => "Old Title" );
+        .RuleFor(x => x.Sub, f => f.Random.Guid())
+        .RuleFor(x => x.Email, f => f.Internet.Email())
+        .RuleFor(x => x.FirstName, f => f.Name.FirstName())
+        .RuleFor(x => x.LastName, f => f.Name.LastName())
+        .RuleFor(x => x.Password, f => f.Internet.Password())
+        .RuleFor(x => x.Salt, f => f.Random.AlphaNumeric(32))
+        .RuleFor(x => x.Status, _ => (short)AccountStatus.Active)
+        .RuleFor(x => x.CreatedAt, f => f.Date.Past(2))
+        .RuleFor(x => x.UpdatedAt, (f, user) => f.Date.Between(user.CreatedAt, DateTime.UtcNow))
+        .RuleFor(x => x.JobTitle, (_, _) => "Old Title");
 
     public static Faker<OrganisationEntity> Organisation => new Faker<OrganisationEntity>()
-        .RuleFor( x => x.Id, f => f.Random.Guid() )
-        .RuleFor( x => x.Name, f => f.Company.CompanyName() )
-        .RuleFor( x => x.Category, _ => "001" )
-        .RuleFor( x => x.Status, _ => (int)OrganisationStatus.Open )
-        .RuleFor( x => x.CreatedAt, f => f.Date.Past( 2 ) )
-        .RuleFor( x => x.UpdatedAt, ( f, org ) => f.Date.Between( org.CreatedAt, DateTime.UtcNow ) );
+        .RuleFor(x => x.Id, f => f.Random.Guid())
+        .RuleFor(x => x.Name, f => f.Company.CompanyName())
+        .RuleFor(x => x.Category, _ => "001")
+        .RuleFor(x => x.Status, _ => (int)OrganisationStatus.Open)
+        .RuleFor(x => x.CreatedAt, f => f.Date.Past(2))
+        .RuleFor(x => x.UpdatedAt, (f, org) => f.Date.Between(org.CreatedAt, DateTime.UtcNow));
 }
