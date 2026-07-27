6c329311a408a78841fb70a04ca6a61cf36f4b11
amjadmahm <amjadmahm@gmail.com>
2026-07-27 15:13:22 +0100

    DSI-8873/8874: Integration Tests > Is Organisation Approver / Get Pending Approval Count (#229)
    Co-authored-by: Amjad Mahmood <amjad.m@hotmail.co.uk>
    Co-authored-by: Amjad Mahmood <amjad.mahmood@education.gov.uk>

--- tests/Dfe.SignIn.TestHelpers/Integration/Data/EntityFaker.cs
+++ tests/Dfe.SignIn.TestHelpers/Integration/Data/EntityFaker.cs
@@ -1,8 +1,14 @@
 using Bogus;
+using Dfe.SignIn.Core.Contracts.Users;
 using Dfe.SignIn.Core.Entities.Directories;
+using Dfe.SignIn.Core.Entities.Organisations;
+using Dfe.SignIn.Core.Public;
 
 namespace Dfe.SignIn.TestHelpers.Integration.Data;
 
+/// <summary>
+/// Provides methods to generate fake entities for testing purposes.
+/// </summary>
 public static class EntityFaker
 {
     public static Faker<UserEntity> User => new Faker<UserEntity>()
@@ -12,8 +18,16 @@ public static class EntityFaker
         .RuleFor(x => x.LastName, f => f.Name.LastName())
         .RuleFor(x => x.Password, f => f.Internet.Password())
         .RuleFor(x => x.Salt, f => f.Random.AlphaNumeric(32))
-        .RuleFor(x => x.Status, _ => (short)1)
+        .RuleFor(x => x.Status, _ => (short)AccountStatus.Active)
         .RuleFor(x => x.CreatedAt, f => f.Date.Past(2))
         .RuleFor(x => x.UpdatedAt, (f, user) => f.Date.Between(user.CreatedAt, DateTime.UtcNow))
         .RuleFor(x => x.JobTitle, (_, _) => "Old Title");
+
+    public static Faker<OrganisationEntity> Organisation => new Faker<OrganisationEntity>()
+        .RuleFor(x => x.Id, f => f.Random.Guid())
+        .RuleFor(x => x.Name, f => f.Company.CompanyName())
+        .RuleFor(x => x.Category, _ => "001")
+        .RuleFor(x => x.Status, _ => (int)OrganisationStatus.Open)
+        .RuleFor(x => x.CreatedAt, f => f.Date.Past(2))
+        .RuleFor(x => x.UpdatedAt, (f, org) => f.Date.Between(org.CreatedAt, DateTime.UtcNow));
 }
