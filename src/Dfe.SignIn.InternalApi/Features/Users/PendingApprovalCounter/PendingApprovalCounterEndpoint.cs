9f58131d81d92ca92c062551078404e4dd9295df
John Rudd <john.rudd@education.gov.uk>
2026-08-03 15:19:28 +0100

    As no tracking

--- src/Dfe.SignIn.InternalApi/Features/Users/PendingApprovalCounter/PendingApprovalCounterEndpoint.cs
+++ src/Dfe.SignIn.InternalApi/Features/Users/PendingApprovalCounter/PendingApprovalCounterEndpoint.cs
@@ -7,14 +7,14 @@ using Microsoft.EntityFrameworkCore;
 namespace Dfe.SignIn.InternalApi.Features.Users.PendingApprovalCounter;
 
 /// <summary>
-/// 
+/// An endpoint exposing user pending approval counts
 /// </summary>
 public class PendingApprovalCounterEndpoint : IEndpoint
 {
     /// <summary>
-    /// 
+    /// Endpoint mapping method holding configuration.
     /// </summary>
-    /// <param name="app"></param>
+    /// <param name="app">The endpoint route builder to map the endpoint to</param>
     public static void Map(IEndpointRouteBuilder app)
     {
         app.MapGet(UsersApiRoutes.PendingApprovalCounter, Handler)
@@ -27,29 +27,33 @@ public class PendingApprovalCounterEndpoint : IEndpoint
     }
 
     /// <summary>
-    /// 
+    /// Changes the name of a user.
     /// </summary>
-    /// <param name="organisationsDbContext"></param>
-    /// <param name="userId"></param>
-    /// <param name="logger"></param>
-    /// <param name="cancellationToken"></param>
-    /// <returns></returns>
+    /// <param name="organisationsDbContext">The database context to use for accessing user data.</param>
+    /// <param name="userId">The userId that the request belongs to</param>
+    /// <param name="logger">The logger to use for logging information.</param>
+    /// <param name="cancellationToken">The cancellation token.</param>
+    /// <returns>The result of the operation.</returns>
     public static async Task<PendingApprovalCountResponse> Handler(
         DbOrganisationsContext organisationsDbContext,
         Guid userId,
         ILogger<PendingApprovalCounterEndpoint> logger,
         CancellationToken cancellationToken)
     {
-        var orgIds = await organisationsDbContext.UserOrganisations.Include(x => x.Organisation)
+        var orgIds = await organisationsDbContext.UserOrganisations
+                   .AsNoTracking()
+                   .Include(x => x.Organisation)
                    .Where(x => x.UserId == userId && x.RoleId == OrganisationRoles.Approver.Id)
                    .Select(x => x.OrganisationId)
                    .ToListAsync(cancellationToken);
 
         var pendingServiceNotificationCount = await organisationsDbContext.UserServiceRequests
+            .AsNoTracking()
             .CountAsync(x => orgIds.Contains(x.OrganisationId) && !x.ActionedAt.HasValue, cancellationToken);
 
         var pendingOrganisationNotificationCount = await organisationsDbContext.UserOrganisationRequests
-      .CountAsync(x => orgIds.Contains(x.OrganisationId) && !x.ActionedAt.HasValue, cancellationToken);
+            .AsNoTracking()
+            .CountAsync(x => orgIds.Contains(x.OrganisationId) && !x.ActionedAt.HasValue, cancellationToken);
 
         var pendingCount = pendingServiceNotificationCount + pendingOrganisationNotificationCount;
 
