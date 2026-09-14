5616a629c9a5eb98291197403c3716c11a2095c4
John Rudd <john.rudd@education.gov.uk>
2026-09-14 14:41:02 +0100

    Removed commenteed out code

--- src/Dfe.SignIn.Fn.AuthExtensions/Program.cs
+++ src/Dfe.SignIn.Fn.AuthExtensions/Program.cs
@@ -36,9 +36,6 @@ builder.Services
 builder.Services
     .AddInteractionFramework();
 
-//IEnumerable<NodeApiName> requiredNodeApiNames = [
-//    NodeApiName.Access,  NodeApiName.Directories,  NodeApiName.Organisations, NodeApiName.Search];
-
 // Get token credential for making API requests to Node APIs.
 var tokenCredential = TokenCredentialHelpers.CreateFromConfiguration(
     builder.Configuration.GetRequiredSection("InternalApiClient")
@@ -47,7 +44,6 @@ var tokenCredential = TokenCredentialHelpers.CreateFromConfiguration(
 builder.Services
     .Configure<InternalApiClientOptions>(builder.Configuration.GetRequiredSection("InternalApiClient"))
     .SetupInternalApiClient(tokenCredential)
-    //.SetupNodeApiClient(requiredNodeApiNames, builder.Configuration.GetRequiredSection("InternalApiClient"), tokenCredential)
     .SetupResiliencePipelines(builder.Configuration);
 
 builder.Services
@@ -62,8 +58,6 @@ var azureTokenCredential = new DefaultAzureCredential(azureTokenCredentialOption
 builder.Services
     .AddServiceBusIntegration(builder.Configuration, azureTokenCredential)
     .AddAuditingWithServiceBus(builder.Configuration, builder.Environment);
-
-//builder.Services.AddInteractor<AutoLinkEntraUserToDsiUseCase>();
 
 builder.Services
     .AddUsersApiClient(tokenCredential);
