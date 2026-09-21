e27468f37bde13a9f88dacafeaffe6913b3734a3
John Rudd <john.rudd@education.gov.uk>
2026-09-21 14:52:06 +0100

    Removed user creator feature

--- src/Dfe.SignIn.InternalApi/Features/Users/UsersFeature.cs
+++ src/Dfe.SignIn.InternalApi/Features/Users/UsersFeature.cs
@@ -86,7 +86,6 @@ public static class UsersFeature
 
         services.AddScoped<IRemoveInviteService, RemoveInviteService>();
         services.AddScoped<GenericEmailCheck>();
-        services.AddScoped<IUserCreator, UserCreator>();
 
         // Register class-based endpoints with DI
         NewEndpointRegistry.RegisterServices(services);
